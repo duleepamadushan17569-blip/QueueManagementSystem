5ba41156b1a11693f64fee1ce59a28f008a67b2b
DuleepaMD <duleepamadushan17569@gmail.com>
2026-09-22 04:30:32 +0530

    update frontend queue interface

--- app.js
+++ app.js
@@ -1,33 +1,29 @@
-function getNameReson(){
-    let name=document.getElementById("inputn").value;
-    let reson=document.getElementById("inputr").value;
 
-    document.getElementById("inputn").value="";
-    document.getElementById("inputr").value="";
+function getNameReson() {
+    console.log("APP JS IS WORKING!");
 
+    let name = document.getElementById("inputn").value;
+    let reson = document.getElementById("inputr").value;
+
+    // Get queue list
+    let list = document.getElementById("queuelist");
+
+    // Create new queue item
+    let item = document.createElement("div");
+
+    item.innerHTML = `
+        <p>
+            <b>${name}</b> - ${reson}
+        </p>
+    `;
+
+    // Add item to current queue
+    list.appendChild(item);
+
+    // Clear inputs
+    document.getElementById("inputn").value = "";
+    document.getElementById("inputr").value = "";
 }
+
 function displayQueue(){
-    fetch("/Queue")
-        .then(response=>response.json())
-        .then(data=>{
-            
-            let list=document.getElementById("queueList");
-            list.innerHTML="";
-
-            for(i=0;i<data.length;i++){
-                let item=document.createElement("div");
-
-                item.innerHTML=
-                <p>
-                    <b>$(1+i).${data[i].name}</b>
-                    - ${data[i].reson}
-                </p>
-                ;
-                list.appendChild(item);
-            }
-        });
 }
-
-displayQueue();
-
-setInterval(displayQueue, 1000);
