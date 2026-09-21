f0a3c7d367bcf117ea5c7c659bdfa9cec8ba4be7
DuleepaMD <duleepamadushan17569@gmail.com>
2026-09-22 04:41:25 +0530

    update modifiied Queue Management System

--- main.c
+++ main.c
@@ -1,13 +1,53 @@
 #include<stdio.h>
+#include<string.h>
 #include "queue.h"
 
 int main(){
     queue q;
     initialization(&q);
+    int option;
+    do{
 
-    enqueue(&q, "Kamal", "Doctor");
-    enqueue(&q, "Nimal", "Engineer");
-    enqueue(&q, "Saman", "Teacher");
-    displayAll(&q);
+    
+    printf("__Queue Management System___\n\n");
+    printf("now serving person\n");
+    peak(&q);
+    printf("\n\n");
+    printf("-------------Options------------\n\n");
+    printf("1) Call next person in queue \n2) Add person to queue\n3) Display all in the queue\n4) Exit\n");
+    printf("\nSelect your option(1,2,3,4) :");
+    scanf("%d",&option);
+
+    switch(option){
+        case 1 :
+                deque(&q);
+                  printf("------------\n\n");
+                break;
+        case 2:
+               {
+                char name[20];
+                char reson[20];
+                printf("\nEnter first name:");
+                scanf(" %[^\n]",name);
+                printf("\nEnter reason:");
+                scanf(" %[^\n]",reson);
+                enqueue(&q,name,reson);
+                printf("--------------\n\n");
+                break;
+               }
+        case 3:
+                displayAll(&q);
+                printf("---------------\n\n");
+                break;
+        
+        case 4:
+               break;
+        default:
+                printf("select valid option\n");
+                printf("--------------\n\n");
+
+        }   
+    }while(option !=4);
+    return 0;
 
 }
